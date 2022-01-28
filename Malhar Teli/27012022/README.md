e598ee9b94e15e987db04d0247c5a3755a3ccf77
Malhar Teli <malhar.teli@gmail.com>
2022-01-28 03:06:11 -0500

    assignment four complete

---
+++ Malhar Teli/27012022/README.md
@@ -0,0 +1,6 @@
+#Assignment for 27012022
+
+This one was rough. Errors were not detailed, making it difficult to track stress points in code.
+
+Ironically, the more complex challenge, my Customer API example, runs its Graphql Middleware flawlessly. 
+
---
+++ Malhar Teli/27012022/currency/src/main/java/com/example/currency/repositories/CurrencyRepo.java
@@ -0,0 +1,11 @@
+package com.example.currency.repositories;
+
+import com.example.currency.models.Currency;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface CurrencyRepo extends JpaRepository<Currency, String> {
+
+}
