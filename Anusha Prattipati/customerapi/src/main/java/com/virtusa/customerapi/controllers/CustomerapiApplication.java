8ed5fc58190f013aaea1d3d88b60fdbb1a60545b
satyaanusha <pratipatianusha@gmail.com>
2022-02-01 21:13:04 -0600

    updated

---
+++ Anusha Prattipati/customerapi/src/main/java/com/virtusa/customerapi/controllers/CustomerapiApplication.java
@@ -0,0 +1,14 @@
+package com.virtusa.customerapi.controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CustomerapiApplication 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(CustomerapiApplication.class, args);
+    }
+}
